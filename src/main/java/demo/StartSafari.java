234bb481363bcee4e47dcc4c0350f8cf872b8801
rydq <mandadapu.rao@directlinegroup.co.uk>
2017-12-04 23:28:08 +0000

    Rao - Commited clean code

--- src/main/java/demo/StartSafari.java
+++ src/main/java/demo/StartSafari.java
@@ -15,58 +15,55 @@ import org.openqa.selenium.safari.SafariDriver;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class StartSafari
-{
+public class StartSafari {
 
-    public static void main(String[] args) throws MalformedURLException{
+    public static void main(String[] args) throws MalformedURLException {
 
-// Create object of  DesiredCapabilities class and specify android platform
-        DesiredCapabilities capabilities=DesiredCapabilities.iphone();
+        // Create object of  DesiredCapabilities class and specify android platform
+        DesiredCapabilities capabilities = DesiredCapabilities.iphone();
         capabilities.setCapability("automationName", "XCUITest");
 
 
-// set the capability to execute test in chrome browser
-capabilities.setCapability(MobileCapabilityType.BROWSER_NAME,BrowserType.SAFARI);
+        // set the capability to execute test in chrome browser
+        capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, BrowserType.SAFARI);
 
-// set the capability to execute our test in Android Platform
-capabilities.setCapability(MobileCapabilityType.PLATFORM,Platform.IOS);
+        // set the capability to execute our test in Android Platform
+        capabilities.setCapability(MobileCapabilityType.PLATFORM, Platform.IOS);
 
-// we need to define platform name
- capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,"iOS");
+        // we need to define platform name
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
 
-// Set the device name as well (you can give any name)
-        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"iPhone 7");
-//capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"IPhone 6");
+        // Set the device name as well (you can give any name)
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 7");
+        //capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"IPhone 6");
 
-// set the android version as well 
-capabilities.setCapability(MobileCapabilityType.VERSION,"11.0.2");
+        // set the android version as well 
+        capabilities.setCapability(MobileCapabilityType.VERSION, "11.0.2");
 
-// Create object of URL class and specify the appium server address
-URL url= new URL("http://127.0.0.1:4723/wd/hub");
+        // Create object of URL class and specify the appium server address
+        URL url = new URL("http://127.0.0.1:4723/wd/hub");
 
-// Create object of  AndroidDriver class and pass the url and capability that we created
-//WebDriver driver = new AndroidDriver(url, capabilities);
+        // Create object of  AndroidDriver class and pass the url and capability that we created
+        //WebDriver driver = new AndroidDriver(url, capabilities);
         WebDriver driver = new IOSDriver<WebElement>(url, capabilities);
 
-// Open url
- driver.get("http://www.facebook.com");
+        // Open url
+        driver.get("http://www.facebook.com");
 
-// print the title
- System.out.println("Title "+driver.getTitle());
+        // print the title
+        System.out.println("Title " + driver.getTitle());
 
-// enter username
+        // enter username
         driver.findElement(By.name("email")).sendKeys("rao@gmail.com");
 
-// enter password
- driver.findElement(By.name("pass")).sendKeys("MRushi_0");
+        // enter password
+        driver.findElement(By.name("pass")).sendKeys("MRushi_0");
 
-// click on submit button
+        // click on submit button
         driver.findElement(By.id("u_0_5")).click();
 
-// close the browser
+        // close the browser
         driver.quit();
-
-
 
 
     }
